be3f5a55bf580429efeb0c3ba2cf8930074549af
krish <krishnamaram2@gmail.com>
2023-10-11 23:49:59 +0530

    add

--- k8s-kubeadm/outputs.tf
+++ k8s-kubeadm/outputs.tf
@@ -1,6 +1,3 @@
-output "ssh_to_runner" {
-  value = "ssh ubuntu@${aws_instance.runner_node[0].public_ip}"
-} 
 
 output "ssh_to_master" {
   value = "ssh ubuntu@${aws_instance.master_node[0].public_ip}"
@@ -14,3 +11,6 @@ output "ssh_to_worker-2" {
   value = "ssh ubuntu@${aws_instance.worker_node[1].public_ip}"
 } 
 
+output "ssh_to_runner" {
+  value = "ssh ubuntu@${aws_instance.runner_node[0].public_ip}"
+} 
