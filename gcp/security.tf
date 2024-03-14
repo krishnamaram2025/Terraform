5af7821dc54581c4dfe731f748c388b81c2dd6e7
krish <krishnamaram2@gmail.com>
2024-03-14 12:30:00 +0530

    add

--- gcp/security.tf
+++ gcp/security.tf
@@ -2,9 +2,15 @@
 resource "google_compute_firewall" "myfw" {
   name    = "myfw"
   network = google_compute_network.myvpc.name
+  # Allow specific ports
+  #allow {
+  #  protocol = "tcp"
+  #  ports    = ["22","80","443", "8000"]
+ # }
+  # Allow all traffic
+    # Allow specific ports
   allow {
-    protocol = "tcp"
-    ports    = ["22","80","443", "8000"]
+    protocol = "all"
   }
 source_ranges = ["0.0.0.0/0"]
 }
