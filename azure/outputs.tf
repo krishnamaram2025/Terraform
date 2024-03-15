9d24676a3b514291d761161a24318994257fad26
krish <krishnamaram2@gmail.com>
2024-03-15 09:57:18 +0530

    added

--- azure/outputs.tf
+++ azure/outputs.tf
@@ -1,11 +1,3 @@
-
-
-# output "bastionhost_public_ip" {
-#   value = "eval `ssh-agent` && ssh-add -k ~/.ssh/id_rsa && ssh -A azure-user@${azurerm_linux_virtual_machine.bastion.public_ip_address}"
-# }
-
-# output "webapp_private_ip" {
-#   # value = azurerm_linux_virtual_machine.webapp.private_ip
-#   value = "ssh azure-user${azurerm_linux_virtual_machine.webapp.private_ip}"
-# }
-
+output "bastionhost_public_ip" {
+  ssh azure-user@${azurerm_linux_virtual_machine.bastion.public_ip_address}"
+}
